898699d3a20f8b25cad7fd4f3f36ecb6f2d286e3
falconandrea <falcon.andrea88@gmail.com>
2023-10-29 19:57:05 +0100

    chore: remove code to move to another file

--- 2023-10-Puppy-Raffle/test/PuppyRaffleTest.t.sol
+++ 2023-10-Puppy-Raffle/test/PuppyRaffleTest.t.sol
@@ -4,11 +4,9 @@ pragma experimental ABIEncoderV2;
 
 import {Test, console} from "forge-std/Test.sol";
 import {PuppyRaffle} from "../src/PuppyRaffle.sol";
-import {Attacker} from "../src/Attacker.sol";
 
 contract PuppyRaffleTest is Test {
     PuppyRaffle puppyRaffle;
-    Attacker attacker;
     uint256 entranceFee = 1e18;
     address playerOne = address(1);
     address playerTwo = address(2);
@@ -19,8 +17,6 @@ contract PuppyRaffleTest is Test {
 
     function setUp() public {
         puppyRaffle = new PuppyRaffle(entranceFee, feeAddress, duration);
-        attacker = new Attacker(address(puppyRaffle));
-        vm.deal(address(attacker), 5 ether);
     }
 
     //////////////////////
@@ -208,23 +204,6 @@ contract PuppyRaffleTest is Test {
         vm.roll(block.number + 1);
 
         uint256 expectedPrizeAmount = ((entranceFee * 4) * 20) / 100;
-
-        puppyRaffle.selectWinner();
-        puppyRaffle.withdrawFees();
-        assertEq(address(feeAddress).balance, expectedPrizeAmount);
-    }
-
-    function testWithdrawFees2() public playersEntered {
-        console.log("init", address(puppyRaffle).balance);
-
-        vm.warp(block.timestamp + duration + 1);
-        vm.roll(block.number + 1);
-
-        uint256 expectedPrizeAmount = ((entranceFee * 4) * 20) / 100;
-
-        attacker.SelfDestructAttack();
-
-        console.log("after", address(puppyRaffle).balance);
 
         puppyRaffle.selectWinner();
         puppyRaffle.withdrawFees();
